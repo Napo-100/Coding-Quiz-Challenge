1e7927d0deb55996db7b45398946725a93c0c5c9
Napo <Michaeljnapolitano@gmail.com>
2020-08-27 15:20:30 -0400

    welcome page hides and question page comes in

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,17 @@
 const startButton = document.getElementById('start-btn')
-const questionContainerElements = document.getElementById
-('question-container')
+const questionContainerElements = document.getElementById('question-container')
+const welcomePageElements = document.getElementById('welcome-page')
 
 startButton.addEventListener('click', startGame)
 
 function startGame(){
 console.log("started")
 startButton.classList.add('hide')
-removeElement();
+welcomePageElements.classList.add('hide')
+questionContainerElements.classList.remove('hide')
 }
 
-function removeElement() {
-    document.getElementById("welcome-page").style.display = "none";
-  }
+
 
 function setNextQuestion(){
 
