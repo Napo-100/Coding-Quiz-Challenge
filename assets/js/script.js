25399d65a178a857f4b932c6cdf010b002e721b8
Napo <Michaeljnapolitano@gmail.com>
2020-08-29 15:24:30 -0400

    trying to get scores to log and show

--- assets/js/script.js
+++ assets/js/script.js
@@ -36,9 +36,12 @@ const endGameElements = document.getElementById('end-page')
 const scoreElement = document.getElementById('score')
 const displayEl = document.getElementById('display')
 const displayEl2 = document.getElementById('display2')
+const submitButton = document.getElementById('submit-btn')
+let initialsEl = document.getElementById('initials')
 
 let questionCounter = 0;
-let timeLeft = questions.length * 15;                                                        
+let timeLeft = questions.length * 15;     
+                                              
 
 const answer1 = document.getElementById("btn1");
 const answer2 = document.getElementById("btn2");
@@ -50,24 +53,16 @@ const answer4 = document.getElementById("btn4");
 // Timer countdown from 75 seconds
 function countDown() {
                             
-
-    var timeInterval = setInterval(function(){
-
         if(timeLeft > 0){
             timerEl.textContent = "Timer:  " + timeLeft;
             timeLeft--
         }
-        
         else {
-            timerEl.textContent = "Timer:  " + timeLeft;
-            clearInterval(timeInterval);
+            timerEl.textContent = "Timer:  " + timeLeft; 
             endGame();
         }
-        
+    }
 
-    }, 1000);
-        
-}
 
 
 var createQuestionElement = function(index) {
@@ -92,18 +87,20 @@ var checkAnswer = function(event) {
         displayEl.textContent = "-----------Correct!-----------"
     } else {
         displayEl.classList.add('hide')
-        displayEl2.textContent = "-----------Wrong!-----------" 
+        displayEl2.textContent = "-----------Wrong!-----------"
+        timeLeft -= 15;
     }
     
     questionCounter++;
-    
+    if(questionCounter === questions.length){
+        endGame();
+    } else {
     createQuestionElement();
 }
-
-
-
+}
 
 var startGame = function(){
+    timeInterval = setInterval(countDown, 1000);
     startButton.classList.add('hide')
     // shuffledQuestions = questions.sort(() => Math.random() - .5)
     // currentQuestionIndex = 0
@@ -114,17 +111,70 @@ var startGame = function(){
     createQuestionElement();
     }
 
-  var endGame = function(){
+var endGame = function(){
+    clearInterval(timeInterval);
     questionContainerElement.classList.add('hide')
        endGameElements.classList.remove('hide')
        scoreElement.textContent = "Your final score is " + timeLeft;
-  }  
+       timerEl.classList.add('hide')
+       timerInterval = setInterval(countDown, 1000);
 
+       setTimeout(function() {
+           displayEl.setAttribute("class", "hide");
+       }, 2000);
+       setTimeout(function() {
+           displayEl2.setAttribute("class", "hide");
+       }, 2000);
+   
+  }
+
+  function saveScore(){
+
+    var ID = initialsEl.value.trim()
+
+    if (ID !== "") {
+        var highScores =
+        JSON.parse(window.localStorage.getItem("newScores")) || [];
+
+        var newTime = {
+            score: timeLeft,
+            intials: ID,
+        };
+
+    highScores.push(newTime);
+    localStorage.setItem("newScores", JSON.stringify(highScores));
+}
+
+printHighScores()
+  }
+
+function printHighScores() {
+    scoreElement.setAttribute("class", "hide")
+    endGameElements.setAttribute("class", "hide")
+        // either get scores from localstorage or set to empty array
+    var highscores = JSON.parse(window.localStorage.getItem("newScores")) || [];
+
+    // Uses a sort function to place higher scores on top by comparing each score before being plugged into the forEach function
+    highscores.sort(function(a, b) {
+        return b.score - a.score;
+    });
+
+    highscores.forEach(function(score) {
+        // Will make each indiviual input for scores be placed in as initials and scores
+        var liTag = document.createElement("li");
+        liTag.textContent = score.initials + " " + score.score;
+
+        // with plug in the list elements into the ordered list element in html via newScores id
+        var olEl = document.getElementById("newScores");
+        olEl.appendChild(liTag);
+    });
+}
+
+  
 
 startButton.addEventListener('click', startGame)
 answer1.addEventListener("click", checkAnswer)
 answer2.addEventListener("click", checkAnswer)
 answer3.addEventListener("click", checkAnswer)
 answer4.addEventListener("click", checkAnswer)
-
-console.log(questionCounter)
+submitButton.addEventListener('click', saveScore)
